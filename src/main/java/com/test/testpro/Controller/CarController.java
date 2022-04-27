1648c0783fe5aefd9b7bdf66b201b07cb6e53faf
Ahmad Arafat <Ahmad.Arafat@exalt.ps>
2022-04-27 16:30:16 +0300

    deploy app and sql

--- src/main/java/com/test/testpro/Controller/CarController.java
+++ src/main/java/com/test/testpro/Controller/CarController.java
@@ -3,11 +3,7 @@ package com.test.testpro.Controller;
 import com.test.testpro.exception.ApiRequestException;
 import com.test.testpro.model.Car;
 import com.test.testpro.service.CarService;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
@@ -15,58 +11,35 @@ import java.util.List;
 @RestController
 public class CarController {
 
-    private  CarService carService;
+    private CarService carService;
+
     public CarController(CarService carService) {
         this.carService = carService;
     }
 
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to this site";
+    }
 
-    @GetMapping("/getCars")
+
+    @GetMapping("/cars")
     public List<Car> car() {
-       return carService.findAllByRent(false);
+        return carService.findAll();
     }
 
 
-    @GetMapping("/rent/{id}/{rentBy}/{rentalPeriod}")
-    public String rent(@PathVariable int id, @PathVariable String rentBy, @PathVariable String rentalPeriod, HttpServletResponse response) {
-        try {
-
-
-            List<Car> l = carService.findAllByRent(false);
-            carService.rent(l, id, rentBy, rentalPeriod);
-            return "Your rent have been done";
-        }
-              catch (Exception exc) {
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "Id not found", exc);
-        }
-        }
-
-    Thread R1= new Thread(() -> {
-        System.out.println("Thread 1 get list");
-        List<Car> l= carService.findAllByRent(false);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("Thread 1 try to rent");
-        carService.rent(l,1,"Ahmad","3 days");
-    });
-
-    Thread R2= new Thread(() -> {
-        System.out.println("Thread 2 get list");
-        List<Car>  l2= carService.findAllByRent(false);
-        System.out.println("Thread 2 try to rent");
-        carService.rent(l2,1,"Ali","2 Days");
-    });
-
-
-    @GetMapping(value = "/test")
-    public  void te() {
-        R1.start();
-        R2.start();
+    @PutMapping("/rent/{id}/{rentBy}/{rentalPeriod}")
+    public Car rent(@PathVariable int id, @PathVariable String rentBy, @PathVariable String rentalPeriod, HttpServletResponse response) {
+        List<Car> l = carService.findAllByRent(false);
+        return carService.rent(l, id, rentBy, rentalPeriod);
     }
+
+    @PostMapping("car")
+    public Car add(@RequestBody Car car) {
+        return carService.save(car);
+    }
+
     @GetMapping("/testexception")
     public List<Car> testException() {
         throw new ApiRequestException("Test Exception Handling");
